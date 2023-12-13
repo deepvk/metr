5ab6e5d7056bb30f09a5d4f81dc268f2c268ad94
Alexander Varlamov <varlamow.sas@gmail.com>
2023-12-13 23:18:10 +0300

    Updated requirements

---
+++ scripts/tree_ring_test.sh
@@ -0,0 +1,8 @@
+python -m tree_ring_watermark.run_tree_ring_watermark \
+    --run_name no_attack \
+    --w_channel 3 \
+    --w_pattern ring \
+    --start 0 \
+    --end 100 \
+    --reference_model ViT-g-14 \
+    --reference_model_pretrain laion2b_s12b_b42k
