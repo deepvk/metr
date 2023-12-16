f47c6b23dd524bf90db351fff5dc08ce0fe0a68b
Alexander Varlamov <varlamow.sas@gmail.com>
2023-12-17 02:00:39 +0300

    script works with accelerate

--- scripts/tree_ring_test.sh
+++ scripts/tree_ring_test.sh
@@ -1,8 +1,10 @@
-python -m tree_ring_watermark.run_tree_ring_watermark \
+accelerate launch -m tree_ring_watermark.run_tree_ring_watermark \
     --run_name no_attack \
     --w_channel 3 \
     --w_pattern ring \
     --start 0 \
-    --end 100 \
+    --end 10 \
+    --num_inference_steps 1000 \
     --reference_model ViT-g-14 \
-    --reference_model_pretrain laion2b_s12b_b42k
+    --reference_model_pretrain laion2b_s12b_b42k \
+    # --with_tracking 
