2b0b2c2feea9f5201bdffc85deab85c676809457
Alphonsce <varlamov.al@phystech.edu>
2024-01-02 23:07:09 +0300

    started working on VAE and diffusion attacks

---
+++ attacks/scripts/install.sh
@@ -0,0 +1,5 @@
+# you will need gdown and svn
+
+export PATH_TO_OPEN_CLIP=/data/varlamov_a_data/miniconda3/envs/attack_wm/lib/python3.10/site-packages/tree_ring_watermark/open_clip
+
+svn export --force https://github.com/Alphonsce/tree-ring-watermark/trunk/src/tree_ring_watermark/open_clip $PATH_TO_OPEN_CLIP
---
+++ scripts/create_images.sh
@@ -0,0 +1,1 @@
+accelerate launch -m tree_ring_watermark.run_tree_ring_watermark --run_name image_generation --w_channel 3 --w_pattern ring --start 0 --end 1000 --reference_model ViT-g-14 --reference_model_pretrain laion2b_s12b_b42k --save_locally
