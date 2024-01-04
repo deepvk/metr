472e37a9740d6cf1739388c20abfa917a48bf864
Alphonsce <varlamov.al@phystech.edu>
2024-01-04 23:47:42 +0300

    fixed syntax error in sh scripts

--- attacks/scripts/vae2018attacks.sh
+++ attacks/scripts/vae2018attacks.sh
@@ -1,4 +1,4 @@
-for QUALITY in 1 3 6 9 12 15 20
+for QUALITY in 1 3 6 9 12 15 20; do
   accelerate launch -m tree_ring_watermark.run_tree_ring_watermark_attack \
     --project_name vae_2018_attacks \
     --run_name vae_2018_$QUALITY --w_channel 3 --w_pattern ring --start 0 --end 200 --reference_model ViT-g-14 --reference_model_pretrain laion2b_s12b_b42k \
--- attacks/scripts/vae2020attacks.sh
+++ attacks/scripts/vae2020attacks.sh
@@ -1,8 +1,9 @@
-for QUALITY in 1 3 6 9 12 15 20
+for QUALITY in 1 3 6 9 12 15 20; do
   accelerate launch -m tree_ring_watermark.run_tree_ring_watermark_attack \
     --project_name vae_2020_attacks \
     --run_name vae_2020_$QUALITY --w_channel 3 --w_pattern ring --start 0 --end 200 --reference_model ViT-g-14 --reference_model_pretrain laion2b_s12b_b42k \
     --with_tracking \
     --use_attack --attack_type vae --vae_attack_name cheng2020-anchor \
     --vae_attack_quality $QUALITY
+
 done
