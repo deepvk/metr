251fd4a2503b980843c06d77090ae0cc42cf3ea9
Alphonsce <varlamov.al@phystech.edu>
2024-01-13 18:29:56 +0300

    script adjustments

--- attacks/scripts/big_diff_att_test.sh
+++ attacks/scripts/big_diff_att_test.sh
@@ -1,11 +1,11 @@
-STEPS=10
+START=10
 END=500
-STEP_SIZE=20
+STEP_SIZE=10
 
-for ((STEPS; STEPS<=END; STEPS+=STEP_SIZE)); do
+for ((STEPS=START; STEPS<=END; STEPS+=STEP_SIZE)); do
   accelerate launch -m tree_ring_watermark.run_tree_ring_watermark_attack \
-    --project_name big_diff_attacks \
-    --run_name s_$STEPS --w_channel 3 --w_pattern ring --start 0 --end 1000 --reference_model ViT-g-14 --reference_model_pretrain laion2b_s12b_b42k \
+    --project_name new_big_diff_attacks \
+    --run_name s_$STEPS --w_channel 3 --w_pattern ring --start 0 --end 200 --reference_model ViT-g-14 --reference_model_pretrain laion2b_s12b_b42k \
     --with_tracking \
     --use_attack --attack_type diff \
     --diff_attack_steps $STEPS
