51c6078db7238d88dbe0fae5f7fef0aff1a29571
Alphonsce <varlamov.al@phystech.edu>
2024-01-29 18:32:45 +0300

    removed stable signature from submodule - it is sub of tree-ring

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,17 @@
-# you will need gdown and svn
+# conda install -y svn
 
-TMPDIR=/data/varlamov_a_data/tmpdir pip3 --cache-dir /data/varlamov_a_data/cache_dir install -r /data/varlamov_a_data/dima/requirements.txt
+# TMPDIR=/data/varlamov_a_data/tmpdir pip3 --cache-dir /data/varlamov_a_data/cache_dir install -r /data/varlamov_a_data/dima/requirements.txt
 
-export PATH_TO_OPEN_CLIP=/data/varlamov_a_data/miniconda3/envs/wm_env/lib/python3.10/site-packages/tree_ring_watermark/open_clip
+# export PATH_TO_OPEN_CLIP=/data/varlamov_a_data/miniconda3/envs/stable_tree/lib/python3.10/site-packages/tree_ring_watermark
 
-svn export --force https://github.com/Alphonsce/tree-ring-watermark/trunk/src/tree_ring_watermark/open_clip $PATH_TO_OPEN_CLIP
+# svn export --force https://github.com/Alphonsce/tree-ring-watermark/src/tree_ring_watermark/open_clip $PATH_TO_OPEN_CLIP
+
+# export WORKING_OPEN_CLIP_FOLDER=/data/varlamov_a_data/miniconda3/envs/new_attack/lib/python3.10/site-packages/tree_ring_watermark/open_clip
+
+
+SOURCE_FOLDER="/data/varlamov_a_data/miniconda3/envs/new_attack/lib/python3.10/site-packages/tree_ring_watermark/open_clip"
+DESTINATION_FOLDER="/data/varlamov_a_data/miniconda3/envs/stable_tree/lib/python3.10/site-packages/tree_ring_watermark/open_clip"
+
+cp -r "$SOURCE_FOLDER"/* "$DESTINATION_FOLDER"
+
+echo "Files copied successfully."
---
+++ scripts/move.sh
@@ -0,0 +1,6 @@
+SOURCE="/data/varlamov_a_data/dima/sd2_decoder.pth"
+DESTINATION="/data/varlamov_a_data/miniconda3/envs/stable_tree/lib/python3.10/site-packages/tree_ring_watermark"
+
+cp "$SOURCE" "$DESTINATION"
+
+echo "Files copied successfully."
